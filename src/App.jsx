139d0dae7723df0c1160628756fb455336ef291e
divyanshuaaliwal <divyanshuaaliwal@gmail.com>
2025-03-04 18:24:49 +0530

    Lakshya's Issue Resolved

--- src/App.jsx
+++ src/App.jsx
@@ -101,12 +101,10 @@ import Intercloud_Migration from "./pages/Intercloud";
 import AWS from "./pages/AWS";
 import CloudNative from "./pages/CloudNative";
 
-//AI app
 
 import Ai_development from "./pages/Ai_development"
 import Ai_software from "./pages/Ai_software";
 import Llm_development from "./pages/Llm_development"
-<<<<<<< Updated upstream
 
 
 import Shopify from "./pages/Shopifydevlopment";
@@ -115,10 +113,8 @@ import Opencart from "./pages/OpencartDevelopment";
 import Magento from './pages/MagentoDevelopment'
 import Pestrashop from "./pages/PestrashopDevelopment";
 import Odoo from './pages/OdooDevelopment';
-=======
-import Ai_enterprise from './pages/Ai_enterprise'
 
->>>>>>> Stashed changes
+import Ai_enterprise from './pages/Ai_enterprise'
 
 
 function App() {
@@ -391,7 +387,7 @@ function App() {
       <Route path="/ai-app-development" element={<Ai_development/>}/>
       <Route path="/ai-software-development" element={<Ai_software/>}/>
       <Route path="/large-language-model-development" element={<Llm_development/>}/>
-<<<<<<< Updated upstream
+{/* <<<<<<< Updated upstream */}
 
        {/*E-Commerce*/}
 
@@ -401,10 +397,10 @@ function App() {
       <Route path="/magento-development-services" element={<Magento/>}/>
       <Route path="/prestashop-development-services" element={<Pestrashop/>}/>
       <Route path="/odoo-development-services" element={<Odoo/>}/>
-=======
+{/* ======= */}
       <Route path="/enterprise-ai-chatbot-development" element={<Ai_enterprise/>}/>
       
->>>>>>> Stashed changes
+{/* >>>>>>> Stashed changes */}
 
       </Routes>
 
